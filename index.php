1cfa5d97b7323bdf0a0b95a1f4d1148e00f9bc63
DeveloperG27 <g.pumayalla@hotmail.com>
2018-05-25 08:53:04 +0200

    Aggiunta navigation in tutti i dispositivi

--- index.php
+++ index.php
@@ -107,6 +107,15 @@
 				
 			</section>
 
+			<section class="main__navigation">
+				<ul>
+					<li class="nav__item">Primo</li>
+					<li class="nav__item">Secondo</li>
+					<li class="nav__item">Terzo</li>
+					<li class="nav__item">Quarto</li>
+				</ul>
+			</section>
+
 			<section class="main__pagination">
 				<span>Mostra altro</span>
 			</section>
