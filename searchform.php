cabcaea727bc6ef72cbdd33a47def981e5ea9eeb
DeveloperG27 <g.pumayalla@hotmail.com>
2018-05-28 10:09:15 +0200

    Ho implementato anche gli stili

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="searchform" >
 				<div>
 					<label class="screen-reader-text" for="s"></label>
-					<input type="text" value="" name="s" id="s">
+					<input type="text" value="" name="s" id="s" placeholder="Cerca..">
 					<input type="submit" id="searchsubmit" value="">
 				</div>
 			</form>
