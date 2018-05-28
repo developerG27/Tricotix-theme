596114fd0ad32359fd0c6954bafd55fabb8b5512
DeveloperG27 <g.pumayalla@hotmail.com>
2018-05-28 12:07:54 +0200

    Migliorato searchbar tablet

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" class="searchform" >
-				<div>
+				<div class="searchform">
 					<label class="screen-reader-text" for="s"></label>
 					<input type="text" value="" name="s" id="s" placeholder="Cerca..">
 					<input type="submit" id="searchsubmit" value="">
