2f6da2e94a10941b39cf07217a09b4e61b05f329
DeveloperG27 <g.pumayalla@hotmail.com>
2018-06-15 06:06:50 +0200

    prova

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ get_header();
 				<li class="nav__item">Terzo</li>
 				<li class="nav__item">Quarto</li>
 			</ul> -->
-			<p>Categorie</p>
+			<p class="nav--name__item">Categorie</p>
+			<hr>
 			<?php
       wp_nav_menu(array(  
         'container' => 'ul',
