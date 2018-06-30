4d49d40ec5b6fc87424e57298c5cf4d9c95082e1
DeveloperG27 <g.pumayalla@hotmail.com>
2018-06-30 03:42:30 +0200

    shop desktop completato

---
+++ page-shop.php
@@ -0,0 +1,165 @@
+<?php /* Template Name: Shop */ ?>
+<?php 
+$ricerca = $_GET['s'];
+get_header(); 
+
+?>
+	<div class="box">
+		
+
+		<main class="main">
+			<section class="main__box">
+				<div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>	
+        <div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>
+        <div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>
+        <div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>
+        <div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>
+        <div class="card__shop">
+          <div class="card__image">
+            <img class="card__img--custom"src="https://www.bellezzaebenessereshop.it/wp-content/uploads/2017/04/LABOR.W012.jpg" alt="">
+            <div class="card__primary">
+              <div class="card__name">Phon</div>
+              <div class="card__price">79.99€</div>
+            </div>
+          </div>
+          <div class="card--side">
+            <div class="card__feature">
+              <div class="card__feature--style">400watt</div>
+              <div class="card__feature--style">5 temperature</div>
+              <div class="card__feature--style">3 velocità</div>
+              <div class="card__feature--style">499 grammi</div>
+              <div class="card__feature--style">3 metri cavo</div>
+              <div class="card__feature--style">ionico</div>
+            </div>
+            <div class="card__button">
+              <span class="card__button--style">Compra</span>
+            </div>
+          </div>
+        </div>	
+        
+        
+        
+			</section>
+
+			<section class="main__navigation">
+				<ul>
+					<li class="nav__item">Primo</li>
+					<li class="nav__item">Secondo</li>
+					<li class="nav__item">Terzo</li>
+					<li class="nav__item">Quarto</li>
+				</ul>
+			</section>
+
+			<section class="main__pagination">
+				<span>Mostra altro</span>
+			</section>
+		</main>
+	</div>
+</body>
+
+</html>
